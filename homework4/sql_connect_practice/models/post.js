83e05f4d94e09f18cbad7e86951efdfa72a94d26
Um seoyoung <tjdud0123@naver.com>
2020-06-02 19:00:01 +0900

    h4 m3 post 관련 모델파일 수정

--- homework4/sql_connect_practice/models/post.js
+++ homework4/sql_connect_practice/models/post.js
@@ -1,19 +1,82 @@
-module.exports = [{
-        postIdx: 0,
-        authorId: 'tjdud0123',
-        title: '솝트 솝트',
-        content: 'tjdud0123@gmail.com'
+const pool = require('../modules/pool');
+const table = 'post';
+
+const post = {
+    create: async (authorId, title, content) => {
+        const fields = 'authorId, title, content';
+        const questions = `?, ?, ?`;
+        const values = [authorId, title, content];
+        const query = `INSERT INTO ${table}(${fields}) VALUES(${questions})`;
+        try {
+            const result = await pool.queryParamArr(query, values);
+            // console.log(result) 객체 반환, 데이터 삽입 실패시 -1 뜨는듯
+            const insertId = result.insertId;
+            return insertId;
+        } catch (err) {
+            if (err.errno == 1062) {
+                console.log('create ERROR : ', err.errno, err.code);
+                return -1;
+            }
+            console.log('create ERROR : ', err);
+            throw err;
+        }
     },
-    {
-        postIdx: 1,
-        authorId: 'dkdkdkdk',
-        title: '나는 제목이다 ',
-        content: '나는 내용이다'
+    getAll: async () => {
+        const query = `SELECT * FROM ${table}`;
+        try {
+            const result = await pool.queryParam(query);
+            console.log(result)
+            return result
+
+        } catch (err) {
+            if (err.errno == 1062) {
+                console.log('getAll ERROR : ', err.errno, err.code);
+                return -1;
+            }
+            console.log('getAll ERROR : ', err);
+            throw err;
+        }
     },
-    {
-        postIdx: 2,
-        authorId: 'koala',
-        title: '알고리즘',
-        content: '문제는 꾸준히 풀즈아'
+    update: async (id, authorId, title, content) => {
+        const query = `UPDATE ${table} SET authorId = "${authorId}", title = "${title}", content = "${content}" WHERE postIdx="${id}"`;
+
+        try {
+            const result = await pool.queryParam(query);
+            console.log(result) // 객체 반환, 데이터 수정 실패시 -1 뜨는듯
+            const insertId = result.insertId;
+            return insertId;
+
+        } catch (err) {
+            console.log('update ERROR : ', err);
+            throw err;
+        }
+    },
+    delete: async (id) => {
+        const query = `DELETE FROM ${table} WHERE postIdx="${id}"`;
+        try {
+            const result = await pool.queryParam(query);
+            console.log(result) // 객체 반환, 데이터 삭제 실패시 -1 뜨는듯
+            const insertId = result.insertId;
+            return insertId;
+
+        } catch (err) {
+            console.log('delete ERROR : ', err);
+            throw err;
+        }
+    },
+    getPostById: async (id) => {
+        const query = `SELECT * FROM ${table} WHERE postIdx="${id}"`;
+
+        try {
+            const post = await pool.queryParam(query);
+            // console.log(user) 배열로 나옴
+            return post;
+
+        } catch (err) {
+            console.log('getPost ERROR : ', err);
+            throw err;
+        }
     }
-];
+}
+
+module.exports = post;
