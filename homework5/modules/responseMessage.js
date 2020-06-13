6f669e49d9375de6f024acc3c3eda79d140609a3
Um seoyoung <tjdud0123@naver.com>
2020-06-13 17:00:29 +0900

    모듈 관련파일작성

---
+++ homework5/modules/responseMessage.js
@@ -0,0 +1,36 @@
+module.exports = {
+    NULL_VALUE: "필요한 값이 없습니다",
+    OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
+
+    // 회원가입
+    CREATED_USER: "회원 가입 성공",
+    DELETE_USER: "회원 탈퇴 성공",
+    ALREADY_ID: "이미 사용중인 아이디입니다.",
+
+    // 로그인
+    LOGIN_SUCCESS: "로그인 성공",
+    LOGIN_FAIL: "로그인 실패",
+    NO_USER: "존재하지 않는 회원입니다.",
+    MISS_MATCH_PW: "비밀번호가 맞지 않습니다.",
+
+    // 프로필 조회
+    READ_PROFILE_SUCCESS: "프로필 조회 성공",
+
+    // 포스트 관련
+    ALL_POST: "모든 게시글 조회 성공",
+    READ_POST: "게시글 조회 성공",
+    CREATED_POST: "게시글 생성 성공",
+    NO_POST_IDX: "해당 게시글 없음",
+    UPDATE_POST: "게시글 수정 완료",
+    DELETE_POST: "게시글 삭제 완료",
+
+    // 인증 관련
+    EMPTY_TOKEN: '토큰 값이 없습니다.',
+    EXPIRED_TOKEN: '토큰 값이 만료되었습니다.',
+    INVALID_TOKEN: '유효하지 않은 토큰값입니다.',
+    AUTH_SUCCESS: '인증에 성공했습니다.',
+    ISSUE_SUCCESS: '새로운 토큰이 생성되었습니다.',
+
+    // 디비 관룐
+    DB_ERROR: "데이터베이스 오류"
+};
