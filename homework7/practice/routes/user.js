c431950f13b0d3beda8449a899f6a41c4a2b7b68
Um seoyoung <tjdud0123@naver.com>
2020-06-15 14:23:39 +0900

    h7 m1

---
+++ homework7/practice/routes/user.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/user');
+const AuthMiddleware = require('../middlewares/auth');
+const upload = require('../modules/multer')
+
+router.post('/signup', UserController.signup);
+router.post('/signin', UserController.signin);
+
+/* 
+    ✔️ update profile
+    METHOD : POST
+    URI : localhost:3000/user/profile
+    REQUEST HEADER : JWT
+    REQUEST BODY : ⭐️image file ⭐️
+    RESPONSE DATA : user profile
+*/
+router.post('/profile', AuthMiddleware.checkToken, upload.single('profile'), UserController.updateProfile);
+
+module.exports = router;
